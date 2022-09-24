0d3101d400a32045c4946cfdba8125a90a27c216
KarenRamirez155 <112497156+KarenRamirez155@users.noreply.github.com>
2022-09-23 20:18:36 -0500

    Add files via upload

---
+++ tallerPunto2/src/tallerpunto2/División.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tallerpunto2;
+
+/**
+ *
+ * @author KAREN RAMIREZ
+ */
+public class División extends Aabstracta{
+ 
+    @Override
+    public int operacionesBasicas(int num1) {
+        return (num1/num1);
+    }
+    
+    
+}
---
+++ tallerPunto2/src/tallerpunto2/Resta.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tallerpunto2;
+
+/**
+ *
+ * @author KAREN RAMIREZ
+ */
+public class Resta extends Aabstracta{
+    @Override
+    public int operacionesBasicas(int num1) {
+       return (num1-num1); 
+    }
+    
+}
